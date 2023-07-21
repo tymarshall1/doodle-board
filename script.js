22a0bb5de2057fc5edf125624df89aab852ede42
Tyler Marshall <60017345+tymarshall1@users.noreply.github.com>
2023-07-21 14:24:03 -0400

    Made it so when you hover over divs they get colored in. Also made the boxes flex to adjust with grid size

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
 function createGrid(size) {
   const gridContainer = document.querySelector("#gridContainer");
-
-  //hardcoded for now... change to variable when need to make bigger grid
   for (let i = 0; i < size; i++) {
     const row = document.createElement("div");
     row.setAttribute("class", "row");
     for (let i = 0; i < size; i++) {
       const squareDiv = document.createElement("div");
       squareDiv.setAttribute("class", "square");
+      squareDiv.addEventListener("mouseover", () => drawOnGrid(squareDiv));
       row.appendChild(squareDiv);
     }
     gridContainer.appendChild(row);
   }
+}
+
+function drawOnGrid(square) {
+  square.classList.add("draw");
 }
 
 createGrid(16);
